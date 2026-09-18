85552fe589a73bf41027b94595ce3789c4f7132a
shayalm09-oss <shayalm09@gmail.com>
2026-09-18 09:15:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
     <style>
     /* שינוי צבע הרקע של כל העמוד */
     .stApp {
-        background-color: #F8FAFC;
+        background-color: #F5F5F7;
     }
     
     /* שינוי צבע הכתב של הכותרת הראשית */
